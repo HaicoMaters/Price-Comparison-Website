0db6ca4c76583b4d210828ba0cdc72df7fd40d4d
HaicoMaters <haicomaters@gmail.com>
2025-01-15 15:23:38 +0000

    Completed the repositiory.cs functions for database interaction

--- Price Comparison Website/Controllers/ProductController.cs
+++ Price Comparison Website/Controllers/ProductController.cs
@@ -8,17 +8,45 @@ namespace Price_Comparison_Website.Controllers
     public class ProductController : Controller
     {
 		private Repository<Product> products;
+		private Repository<Category> categories;
         private readonly IWebHostEnvironment _webHostEnvironment;
 
         public ProductController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             products = new Repository<Product>(context);
+            categories = new Repository<Category>(context);
             _webHostEnvironment = webHostEnvironment;
         }
 
 		public async Task<IActionResult> Index()
         {
             return View(await products.GetAllAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddEdit(int id)
+        {
+            ViewBag.Categories = await categories.GetAllAsync();
+            if (id==0)
+            {
+                ViewBag.Operation = "Add";
+                return View(new Product());
+            }
+            else
+            {
+                Product product = await products.GetByIdAsync(id, new QueryOptions<Product>
+                {
+                    Includes = "Category", Where = p => p.ProductId == id
+                });
+                ViewBag.Operation = "Edit";
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEdit(Product product, int catId)
+        {
+            return View();
         }
     }
 }
--- Price Comparison Website/Models/Repository.cs
+++ Price Comparison Website/Models/Repository.cs
@@ -15,14 +15,17 @@ namespace Price_Comparison_Website.Models
 			_dbSet = context.Set<T>();
 		}
 
-		public Task AddAsync(T entity)
+		public async Task AddAsync(T entity)
 		{
-			throw new NotImplementedException();
+			await _dbSet.AddAsync(entity);
+			await _context.SaveChangesAsync();
 		}
 
-		public Task DeleteAsync(int id)
+		public async Task DeleteAsync(int id)
 		{
-			throw new NotImplementedException();
+			T entity = await _dbSet.FindAsync(id);
+			_dbSet.Remove(entity);
+			await _context.SaveChangesAsync();
 		}
 
 		public async Task<IEnumerable<T>> GetAllAsync()
@@ -30,19 +33,52 @@ namespace Price_Comparison_Website.Models
 			return await _dbSet.ToListAsync();
 		}
 
-		public Task<IEnumerable<T>> GetAllByIdAsync<TKey>(TKey id, string propertyName, QueryOptions<T> options)
+		public async Task<IEnumerable<T>> GetAllByIdAsync<TKey>(TKey id, string propertyName, QueryOptions<T> options)
 		{
-			throw new NotImplementedException();
+			IQueryable<T> query = _dbSet;
+			if (options.HasWhere)
+			{
+				query = query.Where(options.Where);
+			}
+			if (options.HasOrderBy)
+			{
+                query = query.OrderBy(options.OrderBy);
+			}
+			foreach (string includes in options.GetIncludes())
+			{
+				query.Include(includes);
+			}
+
+			query = query.Where(e => EF.Property<TKey>(e, propertyName).Equals(id));
+
+			return await query.ToListAsync();
 		}
 
-		public Task<T> GetByIdAsync(int id, QueryOptions<T> options)
+		public async Task<T> GetByIdAsync(int id, QueryOptions<T> options)
 		{
-			throw new NotImplementedException();
+			IQueryable<T> query = _dbSet;
+			if (options.HasWhere)
+			{
+				query = query.Where(options.Where);
+			}
+			if (options.HasOrderBy)
+			{
+				query = query.OrderBy(options.OrderBy);
+			}
+			foreach (string includes in options.GetIncludes())
+			{
+				query.Include(includes);
+			}
+
+			var key = _context.Model.FindEntityType(typeof(T)).FindPrimaryKey().Properties.FirstOrDefault();
+			string primaryKeyName = key?.Name;
+			return await query.FirstOrDefaultAsync(e => EF.Property<int>(e, primaryKeyName) == id);
 		}
 
-		public Task UpdateAsync(T entity)
+		public async Task UpdateAsync(T entity)
 		{
-			throw new NotImplementedException();
+			_context.Update(entity);
+			await _context.SaveChangesAsync();
 		}
 	}
 }
