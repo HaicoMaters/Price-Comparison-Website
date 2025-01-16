52503da728599b8008d40fb9644bf0319cc45f5d
HaicoMaters <haicomaters@gmail.com>
2025-01-16 17:05:51 +0000

    Updated ViewProduct Page and starting on ability to add and edit price listings

---
+++ Price Comparison Website/Controllers/PriceListingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Price_Comparison_Website.Data;
+using Price_Comparison_Website.Models;
+
+namespace Price_Comparison_Website.Controllers
+{
+	public class PriceListingController : Controller
+	{
+		private Repository<PriceListing> priceListings;
+		private readonly IWebHostEnvironment _webHostEnvironment;
+
+		public PriceListingController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+		{
+			priceListings = new Repository<PriceListing>(context);
+			_webHostEnvironment = webHostEnvironment;
+		}
+
+		public IActionResult Index()
+		{
+			return RedirectToAction("Index", "Home");
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> AddEdit(int id)
+		{
+
+			return View();
+		}
+	}
+}
