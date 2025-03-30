3d28bcf37532672df258b1365d546bc155bf6bd9
HaicoMaters <haicomaters@gmail.com>
2025-03-30 15:33:57 +0100

    Added Tests for price parser factory

--- Price Comparison Website/Services/WebScraping/PriceParserFactory.cs
+++ Price Comparison Website/Services/WebScraping/PriceParserFactory.cs
@@ -9,19 +9,7 @@ namespace Price_Comparison_Website.Services.WebScraping
 {
     public class PriceParserFactory : IPriceParserFactory
     {
-        /*
-        Add this later to extensions for singleton logic and handling the factory correctly:
-                        services.AddTransient<IPriceParser, AmazonParser>();
-                        services.AddTransient<IPriceParser, EbayParser>();
-
-                        services.AddSingleton<PriceParserFactory>(sp =>
-                        {
-                            var parsers = sp.GetServices<IPriceParser>();
-                            return new PriceParserFactory(parsers);
-                        });
-        */
-
-        private readonly Dictionary<string, IPriceParser> _parsers;
+        private readonly Dictionary<string, IPriceParser> _parsers; // domain and parser
 
         public PriceParserFactory(IEnumerable<IPriceParser> parsers)
         {
